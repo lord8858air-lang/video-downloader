88a433d2f7ce59580c2ca79e17c87594362c33b0
lord8858air-lang <lord8858air@gmail.com>
2025-12-30 13:04:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🚀 Flask App Live!</h1>
+    <p>Congratulations 🎉</p>
+    <p>Your GitHub + Render deployment is successful.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
