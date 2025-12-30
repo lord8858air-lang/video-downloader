d2e77cd3d3d214443a54df93eac7b66ebeb37977
lord8858air-lang <lord8858air@gmail.com>
2025-12-30 19:28:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    characters = [
-        {
-            "name": "Xiao Yan",
-            "novel": "Battle Through the Heavens",
-            "power": "Dou Sheng",
-            "breakthrough": "Chapter 1648",
-            "age": "24",
-            "manhwa": "Chapter 385",
-            "donghua": "Season 5"
-        }
-    ]
-    return render_template("index.html", characters=characters)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
