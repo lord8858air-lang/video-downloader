d6c5aa1f6c3cfe6325a25dbd7f9c832cd248b10c
lord8858air-lang <lord8858air@gmail.com>
2025-12-30 14:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def index():
     return render_template("index.html", message=message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
